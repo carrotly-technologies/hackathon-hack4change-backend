0446b648b45ed2535d72929889d8919791d9308f
Filip Kostecki <filip.kostecki00@gmail.com>
2025-06-27 23:37:12 +0200

    feat: add userCreate mutation

--- src/user/resolvers/user.resolver.ts
+++ src/user/resolvers/user.resolver.ts
@@ -1,4 +1,4 @@
-import { Args, Query, Resolver } from "@nestjs/graphql";
+import { Args, Mutation, Query, Resolver } from "@nestjs/graphql";
 import { UserService } from "@app/user/services/user.service";
 import { UserObject } from "@app/user/objects/user.object";
 import { UserFindManyInput } from "@app/user/inputs/user-find-many.input";
@@ -11,6 +11,7 @@ import {
   INPUT_PAGINATION,
 } from "@app/common/common.constraints";
 import { UserInput } from "@app/user/inputs/user.input";
+import { UserCreateInput } from "@app/user/inputs/user-create.input";
 
 @Resolver(() => UserObject)
 export class UserResolver {
@@ -19,6 +20,13 @@ export class UserResolver {
   @Query(() => UserObject, { nullable: true })
   async user(@Args(INPUT_KEY) input: UserInput): Promise<UserObject | null> {
     return this.userService.findById(input.id);
+  }
+
+  @Mutation(() => UserObject)
+  async userCreate(
+    @Args(INPUT_KEY) input: UserCreateInput,
+  ): Promise<UserObject> {
+    return this.userService.create(input);
   }
 
   @Query(() => UserPaginationResponse)
